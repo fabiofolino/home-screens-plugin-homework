4db8e06e41fc6f869812054ead6d4d43999182e3
fabiofolino <fabiofolino@users.noreply.github.com>
2026-09-07 18:10:28 -0400

    Add Vite config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  esbuild: {
+    jsx: 'transform',
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+  },
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      formats: ['iife'],
+      name: '__HS_PLUGIN__',
+      fileName: () => 'bundle.js',
+    },
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        exports: 'named',
+        globals: { react: 'React', 'react-dom': 'ReactDOM' },
+      },
+    },
+  },
+});
